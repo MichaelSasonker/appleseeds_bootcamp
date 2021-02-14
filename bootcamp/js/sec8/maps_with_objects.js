f313680bbbb8a02fd8058ca56f75047a584816fa
Michael Sasonker <michaelsas1212@gmail.com>
2021-02-14 11:02:36 +0200

    maps with objects exercise

---
+++ bootcamp/js/sec8/maps_with_objects.js
@@ -0,0 +1,37 @@
+/*****************************************************************************
+* File Name: maps_with_objects.js                                            *
+* Section: 8                                                                 *
+* Developer: Michael Sasonker                                                *
+* Date: 02/14/21 10:30                                                       *
+* Status: done                                                               *
+******************************************************************************/
+
+
+function MainFunction()
+{
+    'use strict';
+
+    let obj1 = {
+        name: "Jhon",
+    }
+
+    let obj2 = {
+        name: "Harry",
+    }
+
+    let obj3 = {
+        name: "Jane",
+    }
+
+    let map_obj = new Map();
+
+    map_obj.set(obj1, 1);
+    map_obj.set(obj2, 2);
+    map_obj.set(obj3, 3);
+
+    for (let obj of map_obj)
+    {
+        console.log(obj[0].name + '-> ' + obj[1]);
+    }
+
+}; MainFunction();
