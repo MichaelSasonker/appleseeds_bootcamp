e3022a6a01c3532fb717792a37128326a06e1c18
Michael Sasonker <michaelsas1212@gmail.com>
2021-03-14 17:56:49 +0200

    boxes 3.1

---
+++ bootcamp/react_dir/src/components/boxes_3.1/box4.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Box4 = () => {
+    return (
+        <div className="box4">
+
+        </div>
+    );
+};
+
+export default Box4;
+
---
+++ bootcamp/react_dir/src/components/boxes_3.1/boxes_3.1.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Box1 from './box1'
+import './boxes.css';
+
+const Boxes = () => {
+    
+    return (
+        <div className="box-cont">
+            {Box1()}
+        </div>
+    );
+};
+
+export default Boxes;
