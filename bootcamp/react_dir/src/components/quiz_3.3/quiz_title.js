fb11d47306368f5b55460bfaa9258246481d3627
Michael Sasonker <michaelsas1212@gmail.com>
2021-03-14 21:27:22 +0200

    quiz 3.3

---
+++ bootcamp/react_dir/src/components/quiz_3.3/quiz_title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const QuizTitle = () => {
+    return (
+        <div className='quiz-title'>
+            How Do You Like Front End?
+        </div>
+    );
+};
+
+export default QuizTitle;
