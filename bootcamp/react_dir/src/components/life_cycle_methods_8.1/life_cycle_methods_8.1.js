27a7d6ec3e1ef1e0b160fcf68efe2f5980fab9c3
Michael Sasonker <michaelsas1212@gmail.com>
2021-03-16 11:22:13 +0200

    life cycle methods 8.1

--- bootcamp/react_dir/src/components/life_cycle_methods_8.1/life_cycle_methods_8.1.js
+++ bootcamp/react_dir/src/components/life_cycle_methods_8.1/life_cycle_methods_8.1.js
@@ -31,3 +31,18 @@ class LifeCycle extends React.Component {
 }
 
 export default LifeCycle;
+
+/* App.js
+import React from 'react';
+import LifeCycle from './components/life_cycle_methods_8.1/life_cycle_methods_8.1'
+
+class App extends React.Component {
+  render() {
+    return(
+        <LifeCycle />
+    );
+  };
+}
+
+export default App;
+*/
