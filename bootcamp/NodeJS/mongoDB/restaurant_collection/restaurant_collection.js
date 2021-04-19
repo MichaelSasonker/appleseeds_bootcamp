df4ab31f30687d2194e535537f16b48e912599cb
Michael Sasonker <michaelsas1212@gmail.com>
2021-04-20 00:38:39 +0300

    15.1

--- bootcamp/NodeJS/mongoDB/restaurant_collection/restaurant_collection.js
+++ bootcamp/NodeJS/mongoDB/restaurant_collection/restaurant_collection.js
@@ -4,139 +4,332 @@ const MongoClient = mongodb.MongoClient;
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'findMyRestaurant';
 
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (err, client) => {
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
     if (err) {
         return console.log('Unable to connect to database!');
     }
 
     const db = client.db(databaseName);
 
-    db.collection('restaurants').insertMany([
-        {
-            name: 'Mirazur',
-            address: {
-                city: 'Menton', 
-                street: '30 Avenue Aristide Briand',
-                coordinates: [-77,564, 40.677]
-            },
-            cuisine: 'French',
-            isKosher: false,
-            reviews: [
-                {
-                    date: '01-01-2020',
-                    score: 5
-                },
-                {
-                    date: '01-01-2021',
-                    score: 3
-                },
-                {
-                    date: '05-04-2021',
-                    score: 4
-                }
-            ]
-        },
-        {
-            name: 'Arpege',
-            address: {
-                city: 'Paris', 
-                street: '84 Rue de Varenne',
-                coordinates: [-72,564, 43.677]
-            },
-            cuisine: 'French',
-            isKosher: false,
-            reviews: [
-                {
-                    date: '04-04-2020',
-                    score: 2
-                },
-                {
-                    date: '20-03-2021',
-                    score: 4
-                },
-                {
-                    date: '18-04-2021',
-                    score: 5
-                }
-            ]
-        },
-        {
-            name: 'Cosme',
-            address: {
-                city: 'New York', 
-                street: '35 E 21st',
-                coordinates: [-42,564, 70.677]
-            },
-            cuisine: 'Mexican',
-            isKosher: true,
-            reviews: [
-                {
-                    date: '14-04-2020',
-                    score: 5
-                },
-                {
-                    date: '05-01-2021',
-                    score: 2
-                },
-                {
-                    date: '06-04-2021',
-                    score: 5
-                }
-            ]
-        },
-        {
-            name: 'Le Bernardin',
-            address: {
-                city: 'New York', 
-                street: '155 West 51st St',
-                coordinates: [-22,564, 10.677]
-            },
-            cuisine: 'Fish',
-            isKosher: false,
-            reviews: [
-                {
-                    date: '10-02-2020',
-                    score: 2
-                },
-                {
-                    date: '06-08-2021',
-                    score: 4
-                },
-                {
-                    date: '16-03-2021',
-                    score: 5
-                }
-            ]
-        },
-        {
-            name: 'Septime',
-            address: {
-                city: 'Paris', 
-                street: '80 Rue de Charonne',
-                coordinates: [-42,564, 70.677]
-            },
-            cuisine: 'Fish',
-            isKosher: true,
-            reviews: [
-                {
-                    date: '15-06-2020',
-                    score: 5
-                },
-                {
-                    date: '26-09-2021',
-                    score: 3
-                },
-                {
-                    date: '02-02-2021',
-                    score: 4
-                }
-            ]
-        }
-    ], (err, result) => {
-        if (err) {
-            return console.log('Unable to insert documents');
-        }
+    // db.collection('restaurants').insertMany([
+    //     {
+    //       name: "bombay",
+    //       address: {
+    //         city: "Holon",
+    //         street: "Herzel 15",
+    //         coordinates: [-77.46574, 40.6774],
+    //       },
+    //       cuisine: "indian",
+    //       kosher: true,
+    //       reviews: [
+    //         {
+    //           date: new Date("2016-01-01"),
+    //           score: 7,
+    //         },
+    //         {
+    //           date: new Date("2019-01-01"),
+    //           score: 3,
+    //         },
+    //         {
+    //           date: new Date("2018-01-01"),
+    //           score: 8,
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       name: "falafel 5 shekels",
+    //       address: {
+    //         city: "Bat-Yam",
+    //         street: "Histradrut 85",
+    //         coordinates: [-70.46574, 10.6774],
+    //       },
+    //       cuisine: "street food",
+    //       kosher: false,
+    //       reviews: [
+    //         {
+    //           date: new Date("2019-01-01"),
+    //           score: 3,
+    //         },
+    //         {
+    //           date: new Date("2016-01-01"),
+    //           score: 8,
+    //         },
+    //         {
+    //           date: new Date("2020-01-01"),
+    //           score: 4,
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       name: "asian delight",
+    //       address: {
+    //         city: "Tel Aviv",
+    //         street: "Balfur 15",
+    //         coordinates: [-10.46574, 30.6774],
+    //       },
+    //       cuisine: "asian",
+    //       kosher: true,
+    //       reviews: [
+    //         {
+    //           date: new Date("2020-01-01"),
+    //           score: 3,
+    //         },
+    //         {
+    //           date: new Date("2018-01-01"),
+    //           score: 8,
+    //         },
+    //         {
+    //           date: new Date("2016-01-01"),
+    //           score: 9,
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       name: "coconut jam",
+    //       address: {
+    //         city: "Tel Aviv",
+    //         street: "Stam Adress 15",
+    //         coordinates: [20.46574, -40.6774],
+    //       },
+    //       cuisine: "african",
+    //       kosher: true,
+    //       reviews: [
+    //         {
+    //           date: new Date("2017-01-01"),
+    //           score: 10,
+    //         },
+    //         {
+    //           date: new Date("2016-01-01"),
+    //           score: 8,
+    //         },
+    //         {
+    //           date: new Date("2019-01-01"),
+    //           score: 6,
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       name: "rice and noodles",
+    //       address: {
+    //         city: "Holon",
+    //         street: "Bla Bla 15",
+    //         coordinates: [20.46574, 10.6774],
+    //       },
+    //       cuisine: "asian",
+    //       kosher: false,
+    //       reviews: [
+    //         {
+    //           date: new Date("2016-01-01"),
+    //           score: 1,
+    //         },
+    //         {
+    //           date: new Date("2019-01-01"),
+    //           score: 6,
+    //         },
+    //         {
+    //           date: new Date("2019-01-01"),
+    //           score: 2,
+    //         },
+    //       ],
+    //     },
+    //     {
+    //       name: "thailand paradise",
+    //       address: {
+    //         city: "Holon",
+    //         street: "Bla 13",
+    //         coordinates: [-77.46574, 40.6774],
+    //       },
+    //       cuisine: "asian",
+    //       kosher: false,
+    //       reviews: [
+    //         {
+    //           date: new Date("2020-01-01"),
+    //           score: 7,
+    //         },
+    //         {
+    //           date: new Date("2019-01-01"),
+    //           score: 6,
+    //         },
+    //         {
+    //           date: new Date("2020-01-01"),
+    //           score: 2,
+    //         },
+    //       ],
+    //     },
+    //   ], (err, result) => {
+    //     if (err) {
+    //         return console.log('Unable to insert documents');
+    //     }
 
-        console.log(result.ops);
-    })
+    //     console.log(result.ops);
+    // })
+
+    //1.1
+    // db.collection('restaurants').find({}).toArray((err, restaurant) => {
+    //     if (err) {
+    //         return console.log("Can't find");
+    //     }
+        
+    //     console.log('-------------------------------------------------------------');
+    //     console.log(restaurant);
+    // });
+
+    //1.2
+    // db.collection('restaurants').find({cuisine: 'asian'}).toArray((err, restaurant) => {
+    //     if (err) {
+    //         return console.log("Can't find");
+    //     }
+        
+    //     console.log('-------------------------------------------------------------');
+    //     console.log(restaurant);
+    // });
+
+    //1.3
+    // db.collection('restaurants').find({kosher: true}).toArray((err, restaurant) => {
+    //     if (err) {
+    //         return console.log("Can't find");
+    //     }
+        
+    //     console.log('-------------------------------------------------------------');
+    //     console.log(restaurant);
+    // });
+
+    //1.4
+    // db.collection('restaurants').find({"address.city": 'Tel Aviv'}).toArray((err, restaurant) => {
+    //     if (err) {
+    //         return console.log("Can't find");
+    //     }
+        
+    //     console.log('-------------------------------------------------------------');
+    //     console.log(restaurant);
+    // });
+
+    //1.5
+    // db.collection('restaurants').find({"address.street": 'Balfur 15'}).toArray((err, restaurant) => {
+    //     if (err) {
+    //         return console.log("Can't find");
+    //     }
+        
+    //     console.log('-------------------------------------------------------------');
+    //     console.log(restaurant);
+    // });
+
+    //1.6
+    // db.collection('restaurants').find({"address.coordinates": -77.46574}).toArray((err, restaurant) => {
+    //     if (err) {
+    //         return console.log("Can't find");
+    //     }
+        
+    //     console.log('-------------------------------------------------------------');
+    //     console.log(restaurant);
+    // });
+
+    // 1.7
+    // db.collection('restaurants').find({}).sort({name : 1});
+
+    // 1.8
+    // db.collection('restaurants').find({}).sort({"address.city" : 1});
+
+    //1.9
+    // db.collection('restaurants').findOneAndUpdate({name: 'bombay'}, {$set: {name: 'new delhi'}}, (err, restaurant) => {
+    //     if (err) {
+    //         return console.log("Can't find");
+    //     }
+
+    //     console.log('-------------------------------------------------------------');
+    //     console.log(restaurant);
+    // });
+
+    // 1.10
+    // db.collection('restaurants').findOneAndUpdate({name: 'new delhi'}, 
+    // {$push: {reviews: {date: new Date("2021-01-01"), score: 2}}}, (err, restaurant) => {
+    //     if (err) {
+    //         return console.log("Can't find");
+    //     }
+        
+    //     console.log('-------------------------------------------------------------');
+    //     console.log(restaurant);
+    // });
+
+    // 1.11
+    // db.collection('restaurants').updateMany({}, 
+    // {$set: {kosher: true}}, (err, restaurant) => {
+    //     if (err) {
+    //         return console.log("Can't find");
+    //     }
+        
+    //     console.log('-------------------------------------------------------------');
+    //     console.log(restaurant);
+    // });
+
+    // 1.12
+    // db.collection('restaurants').findOneAndDelete({name: 'falafel 5 shekels'},(err, restaurant) => {
+    //     if (err) {
+    //         return console.log("Can't find");
+    //     }
+        
+    //     console.log('-------------------------------------------------------------');
+    //     console.log(restaurant);
+    // });
+
+    //1.13 
+    // db.collection('restaurants').deleteMany({},(err, restaurant) => {
+    //     if (err) {
+    //         return console.log("Can't find");
+    //     }
+        
+    //     console.log('-------------------------------------------------------------');
+    //     console.log(restaurant);
+    // });
+
+    //TODO: ask about positional operator!!!
+    //1.14
+    // db.collection('restaurants').updateOne({name: 'asian delight'}, {$inc: {"reviews.$[].score": 2}});
+
+    //1.15
+    // db.collection('restaurants').updateOne({name: 'asian delight'}, {$inc: {"reviews.$[].score": -1}});
+
+
+    //TODO: ask why print is not defined
+    //2.1
+    // db.collection('restaurants').find({}).forEach(restaurant => print(restaurant.name));
+
+    //2.2
+    // db.collection('restaurants').find({}).forEach(restaurant => print(restaurant.address.city));
+
+    //2.3
+    // db.collection('restaurants').find({}).forEach(restaurant => print(restaurant.address.coordinates));
+
+
+    //2.4 
+    //db.collection('restaurants').find({}).forEach(res => res.address.coordinates.forEach(coor => print(coor)));
+
+    //3.1
+    // db.collection('restaurants').find({name: /^b/i}).toArray((err, restaurant) => {
+    //     if (err) {
+    //         return console.log("Can't find");
+    //     }
+
+    //     console.log(restaurant);
+
+    //     //On Robo3T:
+    //     // db.getCollection('restaurants').find({}).forEach(restaurant => {
+    //     //     if (restaurant.name.startsWith('b')) {
+    //     //         print(restaurant);
+    //     //     };
+    //     // });
+    // });
+
+    //3.2
+    // db.collection('restaurants').find().count().then(res => console.log(res));
+    
+    //3.3
+    // db.collection('restaurants').find({"reviews.date": {$gte: new Date("2020-01-01"), $lt: new Date("2020-01-02")}}).toArray((err, res) => {
+    //     if (err) {
+    //         return console.log("Can't find");
+    //     }
+
+    //     console.log(res);
+    // });
+
 })
